f811fc249d74aeb02128d8df23edaf1987086671
Magdalena Sikorska <madzia.sikorska@gmail.com>
2012-05-18 12:35:13 +0200

    fixing stupid mistake

--- lib/xing/client.rb
+++ lib/xing/client.rb
@@ -8,14 +8,14 @@ module Xing
     attr_reader :consumer_token, :consumer_secret, :consumer_options
 
     def initialize(options={})
-      @consumer_token   = options[:consumer_token]
+      @consumer_key     = options[:consumer_key]
       @consumer_secret  = options[:consumer_secret]
       @auth_token       = options[:oauth_token]
       @auth_secret      = options[:oauth_token_secret]
     end
 
     def consumer
-      @consumer ||= ::OAuth::Consumer.new(@consumer_token, @consumer_secret, DEFAULT_OAUTH_OPTIONS)
+      @consumer ||= ::OAuth::Consumer.new(@consumer_key, @consumer_secret, DEFAULT_OAUTH_OPTIONS)
     end
 
     def access_token
